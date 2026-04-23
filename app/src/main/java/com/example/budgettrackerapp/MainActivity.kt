3108955d1818efb8121dba03db90c3a89a798194
ayanzuza <ST10400793@myemeris.edu.za>
2026-04-23 13:56:19 +0200

    Ayabonga: Implemented login validation and AddExpenseActivity structure

--- app/src/main/java/com/example/budgettrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/budgettrackerapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.budgettrackerapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,11 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Button
         val loginBtn = findViewById<Button>(R.id.loginBtn)
 
+        // Input fields
+        val etUsername = findViewById<EditText>(R.id.username)
+        val etPassword = findViewById<EditText>(R.id.password)
+
         loginBtn.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
+            val username = etUsername.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+            // Validation
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter username and password", Toast.LENGTH_SHORT).show()
+            } else {
+                // Navigate to HomeActivity
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
